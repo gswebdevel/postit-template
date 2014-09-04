9d8c4397c87f22b5dca47af29da91d92fba860c4
Greg Smith <gsmith@westmont.edu>
2014-09-04 16:24:12 -0700

    2

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,5 +3,5 @@ class Vote < ActiveRecord::Base
   belongs_to :voteable, polymorphic: true
 
   # comment for now for ease of testing
-  validates_uniqueness_of :user, scope: :voteable
+  validates_uniqueness_of :user, scope: [:voteable_id, :voteable_type]
 end
