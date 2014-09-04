4b19d12a792f1c2f6ece9f817d979d58f44ff9c6
Greg Smith <gsmith@westmont.edu>
2014-09-03 18:10:22 -0700

    “message”

--- db/migrate/20140813162940_change_post_categories.rb
+++ db/migrate/20140813162940_change_post_categories.rb
@@ -1,9 +1,13 @@
 class ChangePostCategories < ActiveRecord::Migration
   def change
-    change_table :post_categories do |t|
-      t.integer  :post_id
+    create_table :post_categories do |t|
+      t.integer :category_id
+      t.integer :post_id
       t.timestamps
+
+
     end
 
   end
+
 end
